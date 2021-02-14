0204cc3e3d0d1fd1277834a368dda7fec080ad85
Thyago Quintas <thyagoquintas@hotmail.com>
2021-02-14 02:55:30 -0300

    Version 0.1 - Initial Stable Version

---
+++ WebCam/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace WebCam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.picWebCam = new System.Windows.Forms.PictureBox();
+            this.cbxCamera = new System.Windows.Forms.ComboBox();
+            this.cbxWide = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pbxMove = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picWebCam)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxMove)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picWebCam
+            // 
+            this.picWebCam.Location = new System.Drawing.Point(-1, 0);
+            this.picWebCam.Margin = new System.Windows.Forms.Padding(0);
+            this.picWebCam.Name = "picWebCam";
+            this.picWebCam.Size = new System.Drawing.Size(1, 1);
+            this.picWebCam.TabIndex = 0;
+            this.picWebCam.TabStop = false;
+            // 
+            // cbxCamera
+            // 
+            this.cbxCamera.FormattingEnabled = true;
+            this.cbxCamera.Location = new System.Drawing.Point(12, 12);
+            this.cbxCamera.Name = "cbxCamera";
+            this.cbxCamera.Size = new System.Drawing.Size(196, 21);
+            this.cbxCamera.TabIndex = 1;
+            // 
+            // cbxWide
+            // 
+            this.cbxWide.AutoSize = true;
+            this.cbxWide.Location = new System.Drawing.Point(227, 15);
+            this.cbxWide.Name = "cbxWide";
+            this.cbxWide.Size = new System.Drawing.Size(47, 17);
+            this.cbxWide.TabIndex = 2;
+            this.cbxWide.Text = "16:9";
+            this.cbxWide.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(262, 42);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Ativar Camera";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pbxMove
+            // 
+            this.pbxMove.BackColor = System.Drawing.Color.Transparent;
+            this.pbxMove.Image = ((System.Drawing.Image)(resources.GetObject("pbxMove.Image")));
+            this.pbxMove.Location = new System.Drawing.Point(0, 0);
+            this.pbxMove.Margin = new System.Windows.Forms.Padding(0);
+            this.pbxMove.Name = "pbxMove";
+            this.pbxMove.Size = new System.Drawing.Size(4, 4);
+            this.pbxMove.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxMove.TabIndex = 4;
+            this.pbxMove.TabStop = false;
+            this.pbxMove.Visible = false;
+            this.pbxMove.MouseLeave += new System.EventHandler(this.pbxMove_MouseLeave);
+            this.pbxMove.MouseHover += new System.EventHandler(this.pbxMove_MouseHover);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 102);
+            this.Controls.Add(this.pbxMove);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cbxWide);
+            this.Controls.Add(this.cbxCamera);
+            this.Controls.Add(this.picWebCam);
+            this.Name = "Form1";
+            this.Text = "frmWebCam";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            ((System.ComponentModel.ISupportInitialize)(this.picWebCam)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxMove)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picWebCam;
+        private System.Windows.Forms.ComboBox cbxCamera;
+        private System.Windows.Forms.CheckBox cbxWide;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pbxMove;
+    }
+}
+
---
+++ WebCam/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace WebCam
+{
+    public partial class Form1 : Form
+    {
+        public DirectX.Capture.Filter Camera;
+        public DirectX.Capture.Capture CaptureInfo;
+        public DirectX.Capture.Filters CamContainer;
+        Image capturaImagem;
+        Size windowSize;
+        bool wide = false;
+
+        //Movimento de tela
+        private bool mouseDown;
+        private Point lastLocation;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Posicao()
+        {
+            this.StartPosition = FormStartPosition.Manual;
+            foreach (var scrn in Screen.AllScreens)
+            {
+                if (scrn.Bounds.Contains(this.Location))
+                {
+                    this.Location = new Point(scrn.Bounds.Right - this.Width - 10, scrn.Bounds.Bottom - this.Height - 10);
+                    return;
+                }
+            }
+        }
+
+        public void RemoveTitulos()
+        {
+            //Coloca por cima da tela
+            this.TopMost = true;
+
+            //Remove Title Bar
+            this.ControlBox = false;
+            this.Text = String.Empty;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+        }
+
+        public void AtualizaImagem(PictureBox frame)
+        {
+            try
+            {
+                capturaImagem = frame.Image;
+                this.picWebCam.Image = capturaImagem;
+                
+                //Função de movimentação
+                this.pbxMove.Visible = true;
+                this.pbxMove.MouseDown += MouseDown;
+                this.pbxMove.MouseMove += MouseMove;
+                this.pbxMove.MouseUp += MouseUp;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro " + ex.Message);
+            }
+        }
+
+        public void AtivaCamera(int index)
+        {
+            try
+            {
+                //Obtém o dispositivo de entrada do vídeo
+                Camera = CamContainer.VideoInputDevices[index];
+                //Inicializa a Captura usando o dispositivo
+                CaptureInfo = new DirectX.Capture.Capture(Camera, null);
+                //Define a janela de visualização do vídeo
+                CaptureInfo.PreviewWindow = this.picWebCam;
+                //Capturando o tratamento de evento
+                CaptureInfo.FrameCaptureComplete += AtualizaImagem;
+                //Captura o frame do dispositivo
+                CaptureInfo.CaptureFrame();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Cria o filtro da camera
+            CamContainer = new DirectX.Capture.Filters();
+
+            try
+            {
+                int no_of_cam = CamContainer.VideoInputDevices.Count;
+                for (int i = 0; i < no_of_cam; i++)
+                {
+                    cbxCamera.Items.Add(CamContainer.VideoInputDevices[i].Name);
+                }
+                cbxCamera.SelectedIndex = 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message);
+            }
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            //Fixo 320 x 240
+            float width = 320;
+            float height = 240;
+
+            if (wide)
+            {
+                width = 427;
+                height = 240;
+            }
+
+            /**
+             * Ajustar o código para trabalhar com ratio no redimecionamento para a próxima versão
+             *
+             *
+            
+            float ratio = (float)this.Size.Width / ((float)windowSize.Width / (float)windowSize.Height);
+            if(ratio == 1)
+            {
+                ratio = (float)this.Size.Height * ((float)windowSize.Height / (float)windowSize.Width);
+            }
+
+            float width = (float)this.Size.Width * ratio;
+            float height = (float)this.Size.Height * ratio;
+            */
+
+            windowSize = new Size((int)width - 1, (int)height);
+
+            this.Size = windowSize;
+            picWebCam.Size = new Size((int)width, (int)height);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cbxCamera.Visible = false;
+            cbxWide.Visible = false;
+            button1.Visible = false;
+            
+            //Remove titulo da Janela
+            RemoveTitulos();
+
+            //Verifica se está em Wide
+            if (cbxWide.Checked)
+            {
+                wide = true;
+            }
+            //Redimenciona a tela
+            Form1_ResizeEnd(null, null);
+
+            //Ativa a camera Selecionada
+            AtivaCamera(cbxCamera.SelectedIndex);
+
+            //Altera a posição da janela
+            Posicao();
+        }
+
+        private void MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+            lastLocation = e.Location;
+        }
+
+        private void MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown)
+            {
+                this.Location = new Point(
+                    (this.Location.X - lastLocation.X) + e.X, (this.Location.Y - lastLocation.Y) + e.Y);
+
+                this.Update();
+            }
+        }
+
+        private void MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+
+        private void pbxMove_MouseHover(object sender, EventArgs e)
+        {
+            this.pbxMove.Size = new Size(20,20);
+        }
+
+        private void pbxMove_MouseLeave(object sender, EventArgs e)
+        {
+            this.pbxMove.Size = new Size(4, 4);
+        }
+    }
+}
